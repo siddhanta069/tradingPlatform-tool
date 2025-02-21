541d059b557e9fe80d85ec89d2cfad0501a953e5
siddhanta sahoo <siddhanta027z@gmail.com>
2025-02-21 23:20:52 +0530

    ..

--- src/landing_page/home/Awards.jsx
+++ src/landing_page/home/Awards.jsx
@@ -1,8 +1,31 @@
 
 const Awards = () => {
   return (
-    <div>
-      Awards
+    <div className="container">
+      <div className="row">
+        <div className="col-6"> 
+          <div>
+            <h2 className="fw-bold fs-1 mb-5">Trust with confidence</h2>
+              <h4 className="mb-1 ">Customer-first always</h4>
+              <p>That's why 1.5+ crore customers trust Zerodha with ₹4.5+ lakh crores of equity investments and contribute to 15% of daily retail exchange volumes in India.</p>
+          </div>
+          <div>
+            <h4 className="mb-1 ">No spam or gimmicks</h4>
+            <p>No gimmicks, spam, "gamification", or annoying push notifications. High quality apps that you use at your pace, the way you like.</p>
+          </div>
+          <div>
+            <h4 className="mb-1 ">The Zerodha universe</h4>
+            <p>Not just an app, but a whole ecosystem. Our investments in 30+ fintech startups offer you tailored services specific to your needs.</p>
+          </div>
+          <div>
+            <h4 className="mb-1 ">Do better with money</h4>
+            <p>With initiatives like Nudge and Kill Switch, we don't just facilitate transactions, but actively help you do better with your money.</p>
+          </div>
+        </div>
+        <div className="col-6">
+          <img className="img-fluid w-75" src="images/ecosystem.png" alt="ecosystem" />
+        </div>
+      </div>
     </div>
   )
 }
--- src/landing_page/home/Hero.jsx
+++ src/landing_page/home/Hero.jsx
@@ -1,12 +1,13 @@
 const Hero = () => {
   return (
-    <div className= "container p-5">
+    <div className= "container p-5 mb-5">
       <div className="row justify-content-center">
         <img className="img-fluid mb-5" src="images/homeHero.png" alt="Hero Image"  />
-        <h1 className="mt-5">Invest in everything</h1>
-        <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
-        <button className="btn btn-primary fs-5 px-4 d-flex justify-content-center">Sign up for free</button>
-
+        <h1 className="mt-5 d-flex justify-content-center">Invest in everything</h1>
+        <p className="d-flex justify-content-center">Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+        <div className="d-flex justify-content-center">
+        <button className="btn btn-primary fs-5 mb-5 px-4 ">Sign up for free</button>
+        </div>
       </div>
     </div>
   )
